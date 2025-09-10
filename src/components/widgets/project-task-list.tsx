1c553df02c3e0eb8f830a4cbee9a6aa3a3634830
rladydqls99 <rladydqls99@naver.com>
2025-09-11 01:28:22 +0900

    ✨ feat: TaskSchedule -> ProjectTaskList로 이름 변경

---
+++ src/components/widgets/project-task-list.tsx
@@ -0,0 +1,45 @@
+import { progressOptions, projectOptions } from "@/config/select-options";
+import RHFInput from "../rhf/rhf-input";
+import RHFSelect from "../rhf/rhf-select";
+import { Button } from "../ui/button";
+
+const ProjectTaskList = () => {
+  return (
+    <div className="flex gap-2">
+      <RHFSelect
+        name="project"
+        placeholder="project"
+        className="flex-2"
+        options={projectOptions}
+      />
+      <RHFInput
+        name="taskName"
+        placeholder="enter the task name.."
+        className="flex-3"
+      />
+      <RHFInput
+        name="planningTime"
+        placeholder="planning time.."
+        className="flex-1"
+      />
+      <RHFInput
+        name="actionTime"
+        placeholder="action time.."
+        className="flex-1"
+      />
+      <RHFSelect
+        name="progress"
+        className="flex-1"
+        placeholder="progress"
+        options={progressOptions}
+      />
+      <div className="col-span-1 flex gap-1">
+        <Button variant="outline" size="icon">
+          -
+        </Button>
+        <Button size="icon">+</Button>
+      </div>
+    </div>
+  );
+};
+export default ProjectTaskList;

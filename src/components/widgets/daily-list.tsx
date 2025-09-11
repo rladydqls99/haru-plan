569b3752932bfe7da43cc051333045a07d4fa81e
rladydqls99 <rladydqls99@naver.com>
2025-09-11 15:33:49 +0900

    ✨ feat: useFieldArray를 사용해서 form 관리

--- src/components/widgets/daily-list.tsx
+++ src/components/widgets/daily-list.tsx
@@ -1,36 +1,52 @@
 "use client";
 
 import { zodResolver } from "@hookform/resolvers/zod";
-import { useForm } from "react-hook-form";
-import { dailyDefaultList } from "@/models/default-values";
-import { type DailyListSchema, dailyListSchema } from "@/models/validation";
+import { useFieldArray, useForm } from "react-hook-form";
+import { dafaultDailyPlan, defaultDailyProject } from "@/models/default-values";
+import { type DailyPlanSchema, dailyPlanSchema } from "@/models/validation";
 import { Button } from "../ui/button";
 import { Form } from "../ui/form";
 import { Separator } from "../ui/separator";
 import ProjectTaskList from "./project-task-list";
 
 const DailyList = () => {
-  const methods = useForm<DailyListSchema>({
-    defaultValues: dailyDefaultList(),
-    resolver: zodResolver(dailyListSchema),
+  const methods = useForm<DailyPlanSchema>({
+    defaultValues: dafaultDailyPlan(),
+    resolver: zodResolver(dailyPlanSchema),
   });
+
+  const handleSubmit = methods.handleSubmit((data) => console.log(data));
+
+  const { fields, append } = useFieldArray({
+    control: methods.control,
+    name: "projectList",
+  });
+
+  const handleProjectAdd = () => {
+    append(defaultDailyProject());
+    console.log(fields);
+  };
 
   return (
     <Form {...methods}>
-      <form
-        className="w-full space-y-6"
-        onSubmit={methods.handleSubmit((data) => console.log(data))}
-      >
+      <form className="w-full space-y-6" onSubmit={handleSubmit}>
         <div className="flex items-center justify-between m-0">
           <h2 className="text-xl font-semibold text-foreground">
             일일 업무 목록
           </h2>
-          <Button type="submit" size="sm">
-            저장
-          </Button>
+          <div className="space-x-2">
+            <Button onClick={handleProjectAdd} type="button" size="sm">
+              프로젝트 추가
+            </Button>
+            <Button type="submit" size="sm">
+              저장
+            </Button>
+          </div>
         </div>
         <Separator className="mt-4 mb-6" />
-        <ProjectTaskList />
+        {fields.map((field) => (
+          <ProjectTaskList key={field.id} />
+        ))}
       </form>
     </Form>
   );
